766fbcd3578383f9599e8bfb7ef6b3fb7897d71b
MaydayPocket <about.mayday.info@gmail.com>
2024-08-21 14:42:24 -0300

    Create secure.js

---
+++ secure.js
@@ -0,0 +1,16 @@
+const noScreenshot = require('secure-web');
+
+noScreenshot({
+    disableRightClick: true,
+    disableKeyboardShortcuts: true,
+    disableInspectElement: true,
+    disablePrintScreen: true,
+    disableScreenshot: true,
+    disableFunctionKeys: true,
+    disableCtrlF4: true,
+    mouseLeave: true, // required for overlay with mouse leave a browser window
+    mouseEnterAutoHide: false, // required for auto hide overlay with mouse enter a browser window
+    ctrlOverlay: true, 
+    altOverlay: false, // must be pass true for overlay with Alt or Options key press
+    shiftOverlay: false, // must be pass true for overlay with Shift key press
+} , 'custom-overlay-id');
