06cc59364f4f8521d366a12cc2fc466ccdce89f1
MaydayPocket <about.mayday.info@gmail.com>
2024-08-22 21:04:37 -0300

    Update RCsecure.js

--- RCsecure.js
+++ RCsecure.js
@@ -1,4 +1,4 @@
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
-    alert("El clic derecho está desactivado en esta página.");
+    alert("No right click!");
 });
